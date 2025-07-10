9a2511b1254a74990852e0b5c0f20c87db078403
kaito <kaito>
2025-07-10 16:06:47 +0900

    commit14

--- src/hooks/useArchives.ts
+++ src/hooks/useArchives.ts
@@ -4,6 +4,8 @@ import axios from "axios";
 // useArchives.ts
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+console.log("API_BASE_URL:", API_BASE_URL);
+
 export const useMonthlyArchives = () => {
   return useQuery({
     queryKey: ["archives", "monthly"],
